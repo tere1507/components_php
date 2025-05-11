93d389110c3d2fa46e7bfc812fc80eee01a3294a
tere1507 <ttullaguari@gmail.com>
2025-05-11 19:31:32 +0200

    coment

--- 02_data_usuarios.php
+++ 02_data_usuarios.php
@@ -1,5 +1,5 @@
 <?php
 // solo devuleve un array numerico con los nombres de ususarios
- return ['Tere M.', 'Leo U.', 'Manny U.', 'Gaby U.', 'Germy U.', 'Susy C.'];
+ return ['Tere BoniCorneo', 'Leo U.', 'Manny U.', 'Gaby U.', 'Germy U.', 'Susy C.'];
  ?>
 
